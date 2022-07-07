43d679c587a29b048759e580a0fb2720731f6f0d
Lucas Candido <avnserver@gmail.com>
2022-07-07 10:18:10 -0300

    fix: deploy errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./router');
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const dotenv = require("dotenv");
 const cors = require("cors");
 
